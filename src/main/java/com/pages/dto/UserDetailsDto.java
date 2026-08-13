63654e34c4a922e254186eac448a26f65f5100f0
mike oppong <mikeoppong@Mike.local>
2026-08-13 19:04:48 +0200

    fourth

--- src/main/java/com/pages/dto/UserDetailsDto.java
+++ src/main/java/com/pages/dto/UserDetailsDto.java
@@ -40,6 +40,6 @@ public class UserDetailsDto {
     private MultipartFile media;
     private String profileImage;
     private Boolean hasMatchRequest;
-    private boolean acceptedRules;
+    private Boolean acceptedRules;
     private List<PostDto> postDtoList;
 }
